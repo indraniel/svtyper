4d1d7881c3b843ba1c1c7bd42033e3ec85db641d
indraniel <indraniel@gmail.com>
2017-10-26 13:24:33 -0500

    + make the counting work similar to bulkfetch
      - avoid using the iterator as much as possible,  may make things
        slightly faster

--- svtyper/pysamx.pyx
+++ svtyper/pysamx.pyx
@@ -8,48 +8,68 @@ import pysam
 
 # not meant to be used with multiprocessing or parallel code!
 cdef class AlignmentFile(AF):
+    cdef long ccount(self, int tid, int start, int stop):
+        cdef int retval = 0
+        cdef bam1_t *b
+        cdef hts_itr_t *iter = NULL
+        cdef long i = 0
+
+        b = bam_init1()
+        iter = sam_itr_queryi(self.index, tid, start, stop)
+        retval = hts_itr_next(hts_get_bgzfp(self.htsfile), iter, b, self.htsfile)
+        while retval >= 0:
+            if (b.core.flag & (0x4 | 0x100 | 0x200 | 0x400 | 0x800)):
+                # skip read if:
+                # https://github.com/samtools/htslib/blob/master/htslib/sam.h
+                # BAM_FUNMAP | BAM_FSECONDARY | BAM_FQCFAIL | BAM_FDUP | BAM_FSUPPLEMENTARY
+                pass
+            else:
+                i += 1
+            retval = hts_itr_next(hts_get_bgzfp(self.htsfile), iter, b, self.htsfile)
+
+        hts_itr_destroy(iter)
+        bam_destroy1(b)
+
+        if retval == -2:
+            return -2
+
+        return i
+
     def count(self,
               contig=None,
               start=None,
               stop=None,
               region=None,
-              until_eof=False,
-              read_callback="all",
               reference=None,
+              tid=None,
               end=None):
         cdef AlignedSegment read
         cdef long counter = 0
         if not self.is_open:
             raise ValueError("I/O operation on closed file")
 
-        cdef int filter_method = 0
-        if read_callback == "all":
-            filter_method = 1
-        elif read_callback == "nofilter":
-            filter_method = 2
+        cdef int rtid, rstart, rstop, has_coord
 
-        for read in self.fetch(contig=contig,
-                               start=start,
-                               stop=stop,
-                               reference=reference,
-                               end=end,
-                               region=region,
-                               until_eof=until_eof):
-            # apply filter
-            if filter_method == 1:
-                # filter = "all"
-                if (read.flag & (0x4 | 0x100 | 0x200 | 0x400 | 0x800)):
-                    # skip read if:
-                    # https://github.com/samtools/htslib/blob/master/htslib/sam.h
-                    # BAM_FUNMAP | BAM_FSECONDARY | BAM_FQCFAIL | BAM_FDUP | BAM_FSUPPLEMENTARY
-                    continue
-            elif filter_method == 2:
-                # filter = "nofilter"
-                pass
-            else:
-                if not read_callback(read):
-                    continue
-            counter += 1
+        if contig is None:
+            raise ValueError('Need to pass in a valid contig value!')
+        if start is None:
+            raise ValueError('Need to pass in a valid start value!')
+        if stop is None:
+            raise ValueError('Need to pass in a valid stop value!')
+
+        if not self.has_index():
+            raise ValueError("didn't find the index file corresponding to {}".format(self.filename))
+
+        if not self.is_open:
+            raise IOError("Trying to do an I/O operation on a closed file")
+
+        has_coord, rtid, rstart, rstop = self.parse_region(contig, start, stop, region, tid,
+                                                           end=end, reference=reference)
+
+        counter = self.ccount(rtid, start, stop)
+
+        if counter == -2:
+            raise IOError('truncated file')
 
         return counter
 
@@ -139,9 +159,11 @@ cdef class AlignmentFile(AF):
 # 2nd test (475 reads fetch)
 
 # old
+#timeit("foo = pysam.AlignmentFile('../tests/data/NA12878.target_loci.sorted.bam', 'r'); foo.count(contig='2', start=2798760, stop=7009000, read_callback='all')", 'import pysam; import pysamx', number=1000)
 #timeit("foo = pysam.AlignmentFile('../tests/data/NA12878.target_loci.sorted.bam', 'r'); list(foo.fetch(contig='2', start=2798760, stop=7009000))", 'import pysam; import pysamx', number=1000)
 #0.6631929874420166
 
 # new
+#timeit("foo = pysamx.AlignmentFile('../tests/data/NA12878.target_loci.sorted.bam', 'r'); foo.count(contig='2', start=2798760, stop=7009000)", 'import pysam; import pysamx', number=1000)
 #timeit("foo = pysamx.AlignmentFile('../tests/data/NA12878.target_loci.sorted.bam', 'r'); foo.bulkfetch(contig='2', start=2798760, stop=7009000, num_reads=475)", 'import pysam; import pysamx', number=1000)
 #0.6480650901794434
